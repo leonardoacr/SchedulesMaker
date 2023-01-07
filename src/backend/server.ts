4ea5526001ee39ab0777807dada1269cbf77e145
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-01-06 21:56:59 -0300

    deploying on render

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,5 +1,8 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+import * as path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
+console.log('ENV locate test: ' + path.resolve(__dirname, '..', '..', '.env'))
 
 import app from './app';
 
